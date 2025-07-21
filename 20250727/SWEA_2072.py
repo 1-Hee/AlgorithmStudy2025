757f00d45091887e89c0e078db33939ddf6bb6d4
이현석 <endkdl33@naver.com>
2025-07-21 17:35:47 +0900

    img_test

---
+++ 20250727/SWEA_2072.py
@@ -0,0 +1,3 @@
+# SWEA 2072. 홀수만 더하기
+# URL : https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QSEhaA5sDFAUq&categoryId=AV5QSEhaA5sDFAUq&categoryType=CODE&problemTitle=2072&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
+
