e47fa6a9474d6c153d64cbfd7b8aea61848c0eb5
이현석 <endkdl33@naver.com>
2025-07-28 17:18:23 +0900

    9498

--- 20250727/BOJ_9498.py
+++ 20250727/BOJ_9498.py
@@ -1,4 +1,15 @@
+# BOJ 9498. 시험 성적
+# URL : https://www.acmicpc.net/problem/9498
 
-print('hello!')
+a = int(input())
 
-
+if a >= 90:
+    print('A')
+elif 89 >= a >= 80:
+    print('B')
+elif 79 >= a >= 70:
+    print('C')
+elif 69 >= a >= 60:
+    print('D')
+else:
+    print('F')
