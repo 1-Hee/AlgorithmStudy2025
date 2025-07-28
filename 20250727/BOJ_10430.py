c071a3a547be08e7d49941441751af662feaae72
이현석 <endkdl33@naver.com>
2025-07-28 17:40:58 +0900

    7/28_1

--- 20250727/BOJ_10430.py
+++ 20250727/BOJ_10430.py
@@ -1,7 +1,7 @@
 # BOJ 10430. 나머지
 # URL : https://www.acmicpc.net/problem/10430
 
-A, B, C = map(int, input().split()) # 세 수 A, B, C를 " "로 구분된 정수값을 입력받음
+A, B, C = map(int, input().split()) # " "로 구분된 세 수 A, B, C를 정수값으로 입력받음
 
 print((A + B) % C) # 각 주어진 값을 구하는 식을 작성함
 print(((A % C) + (B % C)) % C)
