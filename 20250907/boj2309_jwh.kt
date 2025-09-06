65002bbe5fb736be32e01613a07636a590bc7c82
1-hee <onehee9710@gmail.com>
2025-09-06 23:39:54 +0900

    1-hee : 2025. 09. 06. 주석 추가 및 코드 수정

--- 20250907/boj2309_jwh.kt
+++ 20250907/boj2309_jwh.kt
@@ -11,7 +11,7 @@ private val R:Int = 7
 private var sumTotal:Int = 0
 private var nanJList:List<Int> = listOf()
 
-fun comb(idx:Int, cnt:Int) {
+private fun comb(idx:Int, cnt:Int) {
 
     if(cnt == 2){
         var x = sumTotal
@@ -19,13 +19,13 @@ fun comb(idx:Int, cnt:Int) {
             x -= item
         }
 
-        val sliceSet = slice.toSet()
+        val sliceSet = slice.toSet() // 담아온 배열 2개 짜리를 Set으로 만듦.
 
         if(x == 100){
             val numList:MutableList<Int> = mutableListOf()
             for(i in 0 until N){
                 val num = numbers[i]
-                if(!sliceSet.contains(num)){
+                if(!sliceSet.contains(num)){ // Set에 있는지 확인하려고!
                     numList.add(num)
                 }
             }
@@ -34,6 +34,7 @@ fun comb(idx:Int, cnt:Int) {
         return
     }
 
+    // 조합 로직
     for(i in idx until N){
         slice[cnt] = numbers[i]
         comb(i+1, cnt+1)
@@ -41,19 +42,39 @@ fun comb(idx:Int, cnt:Int) {
 
 }
 
+
+/**
+ * https://www.acmicpc.net/problem/2309
+ * 일곱 난쟁이
+ *
+ * 9C7인 경우에서 각 요소의 합이 100인 경우를 찾아라.
+ */
+
 fun main(){
     System.setIn(FileInputStream("data/250907/boj2309.txt"))
     val br = BufferedReader(InputStreamReader(System.`in`))
 
+    /**
+     * idea : 9C7 이나, 9C2나 수학적으로는 총 경우의 수가 36 임
+     * 조합 공식!
+     * nCr => n! / r!(n-r)!
+     * = 9 x 8 x 7 .. / 7 x 6 .. x (2 x 1)
+     * = 72 / 2 = 36
+     *
+     * = 9 x 8 x 7 ... / 2 x 1 x ( 7 x 6 x 5 ... )
+     * = 72 / 2 = 36
+     */
+
     nanJList = listOf()
     numbers = IntArray(N) { 0 }
-    slice = IntArray(N-R) { 0 }
+    slice = IntArray(N-R) { 0 } // 9C7가 아니라 9C2로 해보려고!
 
     for(i in 0 until N){
         val numX:Int = br.readLine().toInt()
         numbers[i] = numX
     }
 
+    // 전체 합을 구함, 전체 합에서 2개를 뺐을 때, 100이 되는 경우를 찾으려고!
     sumTotal = numbers.sum()
 
     comb(0, 0)
