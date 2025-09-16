cdfadc68094952e4f9c57e8c2e6d51ebc26f74b3
이현석 <endkdl33@naver.com>
2025-09-16 20:26:42 +0900

    아LeeHyeonseok_algo_study_0914_amatta_ccamppak_sorry

--- 20250907/BOJ_6603_HSlee.py
+++ 20250907/BOJ_6603_HSlee.py
@@ -9,13 +9,34 @@ while True:
     S = data[1:] # 만일 0이 아닌 값이면 S에 index 1부터 끝까지 담음
     pick = [0] * 6 # 빈 리스트(로또 번호 추출)
 
-    def backtrack(k, start):
-        if k == 6: # 뽑은 숫자의 갯수가 6자리이면 출력
+    def backtrack(x, s):
+        if x == 6: # 뽑은 숫자의 갯수가 6자리이면 출력
             print(*pick)
+
         else: # 그렇지 않은 경우 그 인덱스 0부터 재귀 돌때마다 1씩 더함
+            for i in range(s, K):
+                pick[x] = S[i]
+                backtrack(x + 1, i + 1)
+
+    backtrack(0, 0) # k : 카드갯수 0부터 시작, start : 시작인덱스 0부터 시작
+    print() # 출력마다 띄어쓰기 위함
+
+while True:
+    data = list(map(int, input().split()))
+    K = data[0]
+    if K == 0:
+        break
+    S = data[1:]
+    pick = [0] * 6
+
+    def backtrack(k, start):
+        if k == 6:
+            print(*pick)
+
+        else:
             for i in range(start, K):
                 pick[k] = S[i]
                 backtrack(k + 1, i + 1)
 
-    backtrack(0, 0) # k : 카드갯수 0부터 시작, start : 시작인덱스 0부터 시작
-    print() # 출력마다 띄어쓰기 위함
+    backtrack(0, 0)
+    print()
--- 20250907/SWEA_2817_HSlee.py
+++ 20250907/SWEA_2817_HSlee.py
@@ -8,15 +8,13 @@ for tc in range(1, 1 + T):
     arr = list(map(int, input().split()))
     count = 0
 
-
-    def dfs(start, s): #
+    def dfs(start, s):
         global count
         if s == K: # s가 K가 되면 count를 더함
             count += 1
 
         for i in range(start, N): # start부터 N까지 반복
             dfs(i + 1, s + arr[i]) # 재귀 돌면서 start는 1 더하고, s에는 값을 하나씩 합함(완탐)
-
 
     dfs(0, 0) # start : index는 0부터 시작, s : 더한 값도 초기값 0부터 시작
     print(f"#{tc} {count}")
